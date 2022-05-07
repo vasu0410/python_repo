67d926aaea40bbbfc92b4c7e589ab7b18d7bd6b5
viperb0t <104640537+viperb0t@users.noreply.github.com>
2022-05-07 20:50:01 +0530

    Update test_best_stock.py

--- Array/test_best_stock.py
+++ Array/test_best_stock.py
@@ -5,7 +5,7 @@ class Teststockpro(unittest.TestCase):
 
     def test_best_profit(self):
         self.assertEqual(best_stock.best_profit([7,1,5,3,6,4]),5)
-        self.assertEqual(best_stock.best_profit([7,6,4,3,1]),0,)
+        self.assertEqual(best_stock.best_profit([7,6,4,3,1]),0)
 
 if __name__ == '__main__':
     unittest.main()
